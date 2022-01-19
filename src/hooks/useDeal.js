999c9910af08eaf902abb2030578649c644c17bc
Tor Kingdon <tor@hearkitty.com>
2022-01-19 01:00:38 -0700

    step 3 is (finally) done and working

---
+++ src/hooks/useDeal.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import uuid from "uuid";
+import axios from "axios";
+
+const useDeal = () => {
+  const [cards, setCards] = useState([]);
+  const addCard = async (url) => {
+    const response = await axios.get(url);
+    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+  };
+
+  return [cards, addCard]
+};
+
+export default useDeal;
